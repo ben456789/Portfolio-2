51738cf63d88678aa5c69226db5d8749e4cab856
Ben Davies <ben@wrangleapp.io>
2020-04-27 23:16:25 +0100

    Updatyes

---
+++ js/core.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+    $('.content-row" .key-feature').each(function (i) {
+        var t = $(this);
+        setTimeout(function () { t.addClass('shimmer'); }, (i + 1) * 100);
+    });
+});
